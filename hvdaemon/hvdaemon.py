af09a267f75c668819c4ff2e2065882fa5cfe0c5
zegrep <zegrep@gmail.com>
2018-09-12 21:57:29 +0200

    freebsd wait for changes to complete

--- hvdaemon/hvdaemon.py
+++ hvdaemon/hvdaemon.py
@@ -6,6 +6,7 @@ import logging
 import os
 import re
 import pyinotify
+import time
 import traceback
 
 import signal
@@ -66,8 +67,10 @@ def process_all_files_from_directory(directory, process_function):
         process_function(path, catch_exceptions=True)
 
 
-
 def process_file(file_path, event=None, catch_exceptions=False):
+    #wait for writing to complete_
+    if hvsystem.is_freebsd():
+        time.spleep(1)
     if not os.path.exists(file_path):
         return
     if not re.match(HV_FILEREGEX, os.path.basename(file_path)):
